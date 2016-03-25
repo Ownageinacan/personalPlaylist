233ba3b401561bb9d8bf66428c10e53ddce36ae3
Shawn Pierpont <spierpon@ycp.edu>
2016-03-24 21:14:08 -0400

    Added several classes with fields and empty methods

---
+++ CS320_Lab03/CS320_Lab03/src/edu/ycp/cs320/lab03/Album.java
@@ -0,0 +1,22 @@
+package edu.ycp.cs320.lab03;
+
+public class Album 
+{
+	private String title;
+	
+	Album(String title)
+	{
+		this.setTitle(title);
+	}
+
+	//getter and setter
+	public String getTitle() 
+	{
+		return title;
+	}
+
+	public void setTitle(String title) 
+	{
+		this.title = title;
+	}
+}
